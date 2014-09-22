bcdc43976ae815da83c663df541f9bd6a76746eb
Neco-Murphy <stogamori@gmail.com>
2014-09-22 15:07:09 -0700

    install bower and add app, send function and fetch

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,39 @@
-// YOUR CODE HERE:
+var app = {
+  init: function(){},
+  send: function(message){
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+  fetch: function(){
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      data: JSON.parse(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message fetched');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to fetch message');
+      }
+    });
+  }
+};
 
+var message = {
+  'username': 'shawndrost',
+  'text': 'hello world',
+  'roomname': '4chan'
+};
